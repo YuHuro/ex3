c2a5ed403766acf0d6e2e623b1745ca8afbd644e
YuHuro <128969811+YuHuro@users.noreply.github.com>
2023-05-26 22:44:24 +0300

    Add files via upload

---
+++ ex3/src/Base.jsx
@@ -0,0 +1,12 @@
+import photo from '../src/img/free-icon-500px-121134.png'
+
+function Base(){
+    return(
+        <div>
+            <h1>Hello</h1>
+            <img src={photo}/>
+        </div>
+    )
+}
+
+export default Base
